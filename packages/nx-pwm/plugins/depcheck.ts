43dbd5a81a3834c117976cc09aa2ec6ce3a6d4b7
Giora Guttsait <giora111@gmail.com>
2022-05-28 01:09:41 +0300

    feat: filter publishable projects in the depcheck plugin

--- packages/nx-pwm/plugins/depcheck.ts
+++ packages/nx-pwm/plugins/depcheck.ts
@@ -1,8 +1,23 @@
-import { ProjectConfiguration } from '@nrwl/devkit';
+import { joinPathFragments, ProjectConfiguration } from '@nrwl/devkit';
+import { fileExists, readJsonFile } from 'nx/src/utils/fileutils';
+import { dirname } from 'path';
 
 export const projectFilePatterns = ['project.json'];
 
-export const registerProjectTargets = (): ProjectConfiguration['targets'] => {
+function isPublishableProject(file: string) {
+  const packageJsonPath = joinPathFragments(dirname(file), 'package.json');
+  return (
+    fileExists(packageJsonPath) && readJsonFile(packageJsonPath).publishConfig
+  );
+}
+
+export const registerProjectTargets = (
+  file: string
+): ProjectConfiguration['targets'] => {
+  if (!isPublishableProject(file)) {
+    return;
+  }
+
   return {
     depcheck: {
       executor: 'nx-pwm:depcheck',
